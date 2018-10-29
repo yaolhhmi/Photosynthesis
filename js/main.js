5035c0ae6c5d7534d1e3d7a3cb9114de16aac2bc
Li Yao <yaol@hhmi.org>
2018-10-29 16:00:10 -0400

    menu not done logo fixed

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*!
+* HHMI photosynthesis interactive v1.0.0
+*====================================================
+*/
+
+"use strict";
+
+//define global variables 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+//function
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/*var App = function () {
+    
+}();*/
+
+
+
+/*$(window).on('resize', function () {
+    $('#navbarNav').collapse('hide');
+ })*/
